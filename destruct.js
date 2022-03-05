04fbd508a291e7a52e53c552594de54b1eb6ff57
Angelica <agervais97@gmail.com>
2022-03-04 22:50:37 -0500

    Mostly complete exercise

---
+++ destruct.js
@@ -0,0 +1,40 @@
+// Object Destructuring 1
+
+//1. console.log(numPlanets); 8
+//2. console.log(yearNeptuneDiscovered); 1846
+
+//Object Destructuring 2
+
+//1. console.log(discoveryYears); {yearNeptuneDiscovered: 8; yearMarsDiscovered: 1659}
+
+//Obejct Destructuring 3
+
+//1. getUserData({firstName:"Alejandro", favorite:"purple"}); "Your name is Alejandro and you like purple."
+//2. getUserData({firstName:"Melissa"});
+//3. getUserData({})
+
+//Array Destructuring 1
+
+//1. console.log(first); Maya
+//2. console.log(second); Marisa
+//3. console.log(third); Chi
+
+//Array Destructuring 2
+
+//1. console.log(raindrops); "Raindrops on roses"
+//2. console.log(whiskers); "whiskers on kittens"
+//3. console.log(aFewOfMyFavoriteThings); ["Bright cooper kettles", "warm woolen mittens", "Brown paper packages tied up with strings"]
+
+//Array Destructuring 3
+
+//1. console.log(numbers); [20, 10, 30]
+
+//ES2015 Object Destructuring
+
+const {numbers:{a, b}} = obj
+
+//ES2015 One-Line Array Swap with Destructuring 
+
+[arr[0], arr[1]] = [arr[1], arr[0]];
+
+const raceResults =  arr =>({ first, second, third, ...rest })
